15180e0fe8e607fd11c18d3ab03eaeddbd464f43
Hector Flores <hecto932@gmail.com>
2017-01-19 16:27:11 -0600

    Construyendo nuestra API

--- src/server/assets.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs')
-
-module.exports.serveStatic = function serveStatic (name, callback) {
-	fs.readFile('./public/' + name, function(err, data){
-		if(err) return callback(err)
-
-		callback(err, data.toString())
-	})
-}
--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,26 @@
 import express from 'express'
 const app = express()
 
+const votes = {} // object, dictionary, hashtable
+
 // Middleware
 app.use(express.static('public'))
 
 // GET /votes
 app.get('/votes', (req, res)  => {
-	res.json([])
+	res.json(votes)
 })
 // POST /vote/<id>
 app.post('/vote/:id', (req, res) => {
+	let id = req.params.id
+	
+	if(votes[id] === undefined)
+		votes[id] = 1
+	else
+		votes[id] += 1
 
+	console.log({ votes : votes[id] })
+	res.json({ votes : votes[id] })
 })
 
 app.listen(3000, () => {
